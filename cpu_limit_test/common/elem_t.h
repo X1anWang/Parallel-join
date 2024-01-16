98aae030b8f03ceec57c546f929eb0426810a7eb
mavrogiannakisap <amavrogi@ucsc.edu>
2024-01-16 10:08:55 -0800

    Adding experiment of oblivious swaps

---
+++ cpu_limit_test/common/elem_t.h
@@ -0,0 +1,50 @@
+#ifndef __COMMON_NODE_T_H
+#define __COMMON_NODE_T_H
+
+#include <assert.h>
+#include <stddef.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+#define ELEM_SIZE 128
+#define ELEM_STRUCT_SIZE 19
+#define DATA_LENGTH 1024
+
+typedef int ojoin_int_type;
+
+typedef struct cpu_elem{
+	int key;
+	char data[DATA_LENGTH];
+} cpu_t;
+
+typedef struct elem {
+    int key;
+    int m0;
+    int m1;
+    int j_order;
+    char data[DATA_LENGTH];
+    bool has_value;
+    bool table_0;
+} elem_t;
+
+typedef struct ele2m {
+    ojoin_int_type key;
+    unsigned char data[4];
+
+    /* Oblivious join stuff*/
+    bool has_value;
+    bool table_0;
+
+    /* Florian's join */
+    ojoin_int_type m0;
+    ojoin_int_type m1;
+    ojoin_int_type true_key;
+    ojoin_int_type idx_start;
+    ojoin_int_type j_order;
+
+    //unsigned char unused[ELEM_SIZE - ELEM_STRUCT_SIZE];
+} ele2m_t;
+
+//static_assert(sizeof(elem_t) == ELEM_SIZE, "Element should be 128 bytes");
+
+#endif /* common/elem_t.h */
