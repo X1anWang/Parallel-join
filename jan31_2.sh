26c73b68cc4a30ba8cf0675b64151f9e5eac76e3
X1anWang <x1anwang@outlook.com>
2024-01-31 06:18:18 +0800

    update

--- jan31_2.sh
+++ jan31_2.sh
@@ -7,9 +7,19 @@ sudo ./host/parallel ./enclave/parallel_enc.signed 32 ~/test1_4.txt ~/test5_4.tx
 
 cd ~/Parallel-join/our_join_for_syn;
 make clean; make;
+./host/parallel ./enclave/parallel_enc.signed 1 ~/input_2power_of_22.txt | tee -a ~/jan31_result_tab2.txt;
 ./host/parallel ./enclave/parallel_enc.signed 2 ~/input_2power_of_22.txt | tee -a ~/jan31_result_tab2.txt;
 ./host/parallel ./enclave/parallel_enc.signed 4 ~/input_2power_of_22.txt | tee -a ~/jan31_result_tab2.txt;
 ./host/parallel ./enclave/parallel_enc.signed 8 ~/input_2power_of_22.txt | tee -a ~/jan31_result_tab2.txt;
 ./host/parallel ./enclave/parallel_enc.signed 16 ~/input_2power_of_22.txt | tee -a ~/jan31_result_tab2.txt;
 ./host/parallel ./enclave/parallel_enc.signed 32 ~/input_2power_of_22.txt | tee -a ~/jan31_result_tab2.txt;
-./host/parallel ./enclave/parallel_enc.signed 1 ~/input_2power_of_22.txt | tee -a ~/jan31_result_tab2.txt;
+
+
+cd ~/Parallel-join/our_join_for_tpch;
+make clean; make;
+./host/parallel ./enclave/parallel_enc.signed 1 ~/test1_1.txt ~/test1_2.txt ~/test1_3.txt | tee -a ~/jan31_result_tab2_tpch.txt;
+./host/parallel ./enclave/parallel_enc.signed 2 ~/test1_1.txt ~/test1_2.txt ~/test1_3.txt | tee -a ~/jan31_result_tab2_tpch.txt;
+./host/parallel ./enclave/parallel_enc.signed 4 ~/test1_1.txt ~/test1_2.txt ~/test1_3.txt | tee -a ~/jan31_result_tab2_tpch.txt;
+./host/parallel ./enclave/parallel_enc.signed 8 ~/test1_1.txt ~/test1_2.txt ~/test1_3.txt | tee -a ~/jan31_result_tab2_tpch.txt;
+./host/parallel ./enclave/parallel_enc.signed 16 ~/test1_1.txt ~/test1_2.txt ~/test1_3.txt | tee -a ~/jan31_result_tab2_tpch.txt;
+./host/parallel ./enclave/parallel_enc.signed 32 ~/test1_1.txt ~/test1_2.txt ~/test1_3.txt | tee -a ~/jan31_result_tab2_tpch.txt;
