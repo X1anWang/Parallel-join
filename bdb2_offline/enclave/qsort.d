9610700b00f4703b5498a326d59e6cafd69fc6d5
X1anWang <x1anwang@outlook.com>
2024-02-01 16:55:10 +0800

    update

---
+++ bdb2_offline/enclave/qsort.d
@@ -0,0 +1,10 @@
+enclave/qsort.o: enclave/qsort.c \
+ /opt/openenclave/share/pkgconfig/../../include/openenclave/3rdparty/libc/alloca.h \
+ /opt/openenclave/share/pkgconfig/../../include/openenclave/3rdparty/libc/bits/alltypes.h \
+ /opt/openenclave/share/pkgconfig/../../include/openenclave/3rdparty/libc/bits/deprecations.h \
+ /opt/openenclave/share/pkgconfig/../../include/openenclave/3rdparty/libc/limits.h \
+ /opt/openenclave/share/pkgconfig/../../include/openenclave/3rdparty/libc/features.h \
+ /opt/openenclave/share/pkgconfig/../../include/openenclave/3rdparty/libc/bits/limits.h \
+ /opt/openenclave/share/pkgconfig/../../include/openenclave/3rdparty/libc/stdlib.h \
+ /opt/openenclave/share/pkgconfig/../../include/openenclave/3rdparty/libc/string.h \
+ /opt/openenclave/share/pkgconfig/../../include/openenclave/3rdparty/libc/strings.h
