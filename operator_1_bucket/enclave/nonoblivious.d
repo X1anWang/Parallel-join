4716f0bd3a9c0058f25a6e0185f54f86931da2b2
X1anWang <x1anwang@outlook.com>
2024-02-26 10:45:24 +0800

    supplement

---
+++ operator_1_bucket/enclave/nonoblivious.d
@@ -0,0 +1,35 @@
+enclave/nonoblivious.o: enclave/nonoblivious.c enclave/nonoblivious.h \
+ /opt/openenclave/share/pkgconfig/../../include/openenclave/3rdparty/libc/stddef.h \
+ /opt/openenclave/share/pkgconfig/../../include/openenclave/3rdparty/libc/bits/alltypes.h \
+ /opt/openenclave/share/pkgconfig/../../include/openenclave/3rdparty/libc/bits/deprecations.h \
+ common/elem_t.h \
+ /opt/openenclave/share/pkgconfig/../../include/openenclave/3rdparty/libc/assert.h \
+ /opt/openenclave/share/pkgconfig/../../include/openenclave/3rdparty/libc/features.h \
+ /opt/openenclave/share/pkgconfig/../../include/openenclave/3rdparty/libc/stdbool.h \
+ /opt/openenclave/share/pkgconfig/../../include/openenclave/3rdparty/libc/stdint.h \
+ /opt/openenclave/share/pkgconfig/../../include/openenclave/3rdparty/libc/bits/stdint.h \
+ /opt/openenclave/share/pkgconfig/../../include/openenclave/3rdparty/libc/errno.h \
+ /opt/openenclave/share/pkgconfig/../../include/openenclave/3rdparty/libc/bits/errno.h \
+ /opt/openenclave/share/pkgconfig/../../include/openenclave/3rdparty/libc/stdio.h \
+ /opt/openenclave/share/pkgconfig/../../include/openenclave/3rdparty/libc/string.h \
+ /opt/openenclave/share/pkgconfig/../../include/openenclave/3rdparty/libc/strings.h \
+ common/defs.h common/error.h \
+ /opt/openenclave/share/pkgconfig/../../include/openenclave/bits/result.h \
+ /opt/openenclave/share/pkgconfig/../../include/openenclave/bits/defs.h \
+ /opt/openenclave/share/pkgconfig/../../include/openenclave/bits/types.h \
+ common/util.h \
+ /opt/openenclave/share/pkgconfig/../../include/openenclave/3rdparty/libc/limits.h \
+ /opt/openenclave/share/pkgconfig/../../include/openenclave/3rdparty/libc/bits/limits.h \
+ /opt/openenclave/share/pkgconfig/../../include/openenclave/3rdparty/libc/time.h \
+ enclave/mpi_tls.h \
+ /opt/openenclave/share/pkgconfig/../../include/openenclave/3rdparty/mbedtls/entropy.h \
+ /opt/openenclave/share/pkgconfig/../../include/openenclave/3rdparty/mbedtls/config.h \
+ /opt/openenclave/share/pkgconfig/../../include/openenclave/3rdparty/mbedtls/check_config.h \
+ /opt/openenclave/share/pkgconfig/../../include/openenclave/3rdparty/mbedtls/sha512.h \
+ /opt/openenclave/share/pkgconfig/../../include/openenclave/3rdparty/mbedtls/threading.h \
+ /opt/openenclave/share/pkgconfig/../../include/openenclave/3rdparty/libc/stdlib.h \
+ /opt/openenclave/share/pkgconfig/../../include/openenclave/3rdparty/libc/alloca.h \
+ /opt/openenclave/share/pkgconfig/../../include/openenclave/3rdparty/libc/pthread.h \
+ /opt/openenclave/share/pkgconfig/../../include/openenclave/3rdparty/libc/sched.h \
+ common/ocalls.h enclave/parallel_enc.h enclave/qsort.h enclave/synch.h \
+ enclave/threading.h
